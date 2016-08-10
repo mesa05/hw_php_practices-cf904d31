ed7cd0e90893b02d21c8fee6bd3f48725a1a02aa
Shu Hong Yang <lkg11011@gmail.com>
2016-08-10 14:06:21 +0000

    星星塔第一版

---
+++ tower_Star.php
@@ -0,0 +1,27 @@
+<?php
+    session_start();
+    $towerNum = $_POST["number"];
+    $star="";
+    function towerStar ($towerNum) {
+        for($i = 1 ; $i <= $towerNum ; $i++ ){
+            $star .= "*";
+            echo $star.'<br>';
+        }
+    }
+?>
+
+<html>
+
+<head>
+    <meta charset="utf-8">
+</head>    
+
+<body>
+    <form method="post" action="">
+        請輸入奇數:<input type="text" name="number" value="">
+        <input type="submit" name="btnStart" value="Start">
+    </form>
+    <?php towerStar($towerNum); ?>
+</body>
+
+</html>
